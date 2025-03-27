2d704ae8d0ca378bb9c21a83dc016a3089ffb45f
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-03-27 14:39:08 +0000

    second day doing stuff

--- Scoundrel/Assets/mainScript.cs
+++ Scoundrel/Assets/mainScript.cs
@@ -8,6 +8,8 @@ public class mainScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        System.Random PRand = new System.Random();
+        string difficulty = "Hard";
         string[,] Dungeon1 = new string[4,13];
         string[] Names = new string[13] { "2", "3", "4", "5", "6", "7", "8", "9", "0", "j", "q", "k", "a"};
         string[] Suits = new string[4] { "hearts", "spades", "diamonds", "clubs"};
@@ -18,9 +20,22 @@ public class mainScript : MonoBehaviour
                 Dungeon1[i, j] = ($"{Suits[i]}{Names[j]}");
             }
         }
-        //hearts for health refills, diamonds for weapons, spades for base damage, clubs for (div 5 - 1) * 5 or base damage
+        //hearts for health refills, diamonds for weapons, spades for base damage, clubs for double damage if no health potion was used.
+        string[] dungeonShuffle = new string[26];
+        for (int i = 0;i < 26; i++)
+        {
+            string randomName = Names[PRand.Next(Names.Length)];
+            string randomSuit = Suits[PRand.Next(Suits.Length)];
+            dungeonShuffle[i] = $"{randomSuit}{randomName}";
+        }
+        Debug.Log("hello");
+        Debug.Log(dungeonShuffle[1]);
+        Console.WriteLine(dungeonShuffle);
 
-        
+
+
+
+
 
     }
 
