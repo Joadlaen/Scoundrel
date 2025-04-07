80a10cda84cf1713642276bbc612d07d67c99b75
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-04-07 16:45:47 +0100

    pivoted the project to a new design

--- Scoundrel/Assets/scripts/suits.cs
+++ Scoundrel/Assets/scripts/suits.cs
@@ -8,13 +8,6 @@ namespace Assets.scripts
 {
     internal class suits
     {
-        System.Random PRand = new System.Random();
-        private string suit = "";
-        private int number = 0;
 
-        private void ClubDamage()
-        {
-
-        }
     }
 }
